dfa2d21a2ac3ff87fc4754fcb5f216a9ef993e4a
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-08-21 20:09:35 +0100

    Update kgi.py

--- kgi.py
+++ kgi.py
@@ -1,9 +1,7 @@
-from typing import Any
-
 import torch
 
 
-def uniform(sizes: list[Any], low: float = 0., high: float = 1.):
+def uniform(sizes, low, high):
     """
     Sample from uniform distribution U(low, high)
     :param sizes: sizes of output
@@ -14,31 +12,52 @@ def uniform(sizes: list[Any], low: float = 0., high: float = 1.):
     return torch.rand(sizes) * (high - low) + low
 
 
-def kgi_with_bias(fc: torch.nn.Linear,
-                  knot_low: float = 0.1, knot_high: float = 0.9,
-                  bias_perturb_factor: float = 0.2,
-                  input_size_scaling: float = 3.):
+def kgi_layer(layer, knot_low=0.1, knot_high=0.9,
+              perturb_factor=0.2, kgi_by_bias=True):
     """
-    :param fc: the target `nn.Linear` layer
+    Apply KGI to a layer
+    :param layer: the target `nn.Linear`
     :param knot_low: lower bound of knot positions
     :param knot_high: upper bound of knot positions
-    :param bias_perturb_factor: factor to perturb computed biases
-    :param input_size_scaling: factor to divide input size when sampling weights
-    :return: the re-initialized `nn.Linear` layer
+    :param perturb_factor: factor to perturb the KGI-based weight or bias
+    :param kgi_by_bias: whether to achieve KGI by bias (`True`) or weight (`False`)
+    :return: the re-initialized `nn.Linear`
     """
-    n, m = fc.weight.data.shape
-    # sample weights
-    w_scale = 1 / torch.sqrt(torch.tensor(m) / input_size_scaling)
-    w = uniform([n, m], low=-w_scale, high=w_scale)
+    # get original
+    n, m = layer.weight.data.shape
+    w0 = layer.weight.data
+    b0 = layer.bias.data
+
     # sample knots
     x_knot = uniform(m, low=knot_low, high=knot_high)
-    # compute b
-    b_exact = -torch.mv(w, x_knot)
-    # perturb b
-    b_scale = bias_perturb_factor * w_scale
-    b_perturbed = uniform(n, low=-b_scale, high=b_scale)
-    b = b_exact + b_perturbed
-    # assign to layer
-    fc.weight.data = w
-    fc.bias.data = b
-    return fc
+
+    if kgi_by_bias:
+        # compute b by KGI
+        b_kgi = -torch.mv(w0, x_knot)
+        # perturb b
+        if torch.all(torch.abs(b0) < torch.finfo(b0.dtype).eps):
+            # use He if b0 is zero
+            bound = torch.sqrt(3. / torch.tensor(m))
+            b0 = uniform(n, -bound, bound)
+        # assign to layer
+        layer.bias.data = (1 - perturb_factor) * b_kgi + perturb_factor * b0
+    else:
+        pass
+    return layer
+
+
+def kgi_model(model, knot_low=0.1, knot_high=0.9,
+              perturb_factor=0.2, kgi_by_bias=True):
+    """
+    Apply KGI to a layer
+    :param model: the target `nn.Module`
+    :param knot_low: lower bound of knot positions
+    :param knot_high: upper bound of knot positions
+    :param perturb_factor: factor to perturb the KGI-based weight or bias
+    :param kgi_by_bias: whether to achieve KGI by bias (`True`) or weight (`False`)
+    :return: the re-initialized `nn.Module`
+    """
+    for layer in model.modules():
+        if isinstance(layer, torch.nn.Linear):
+            kgi_layer(layer, knot_low, knot_high,
+                      perturb_factor, kgi_by_bias)
