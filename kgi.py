5a41293430b5c45e308f969e4568971321689ec3
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-08-21 20:45:31 +0100

    Update kgi.py

--- kgi.py
+++ kgi.py
@@ -42,14 +42,22 @@ def kgi_layer(layer, knot_low=0.1, knot_high=0.9,
         # assign to layer
         layer.bias.data = (1 - perturb_factor) * b_kgi + perturb_factor * b0
     else:
-        pass
+        # compute w by KGI
+        x2 = torch.dot(x_knot, x_knot)
+        w_p = -torch.outer(b0, x_knot) / x2  # particular solution
+        w_h = w0 - torch.outer(torch.mv(w0, x_knot), x_knot) / x2  # homogenous solution
+        # combine the two by preserving mean
+        alpha = (w0.sum() - w_p.sum()) / w_h.sum()
+        w_kgi = w_p + alpha * w_h
+        # perturb w
+        layer.weight.data = (1 - perturb_factor) * w_kgi + perturb_factor * w0
     return layer
 
 
 def kgi_model(model, knot_low=0.1, knot_high=0.9,
               perturb_factor=0.2, kgi_by_bias=True):
     """
-    Apply KGI to a layer
+    Apply KGI to a model
     :param model: the target `nn.Module`
     :param knot_low: lower bound of knot positions
     :param knot_high: upper bound of knot positions
